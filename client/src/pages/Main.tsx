329e69ecc36a9a803a343791370d5b3b2339684c
wavescats <wavescats@gmail.com>
2022-09-15 01:34:06 +0900

    [FEAT] Admin / User Wallet Cert

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -2,11 +2,19 @@ import { Link } from "react-router-dom";
 
 function Main() {
   return (
-    <>
+    <div className="container">
+      <br />
+      <h1>Welcome to Simple Asset Transfer Basic !</h1>
+      <p>자산관리 사이트에 오신것을 환영합니다!</p>
+      <Link to="/admin">
+        <div className="btn btn-danger">관리자 지갑 생성</div>
+      </Link>
+      <br />
+      <br />
       <Link to="/users">
         <div className="btn btn-success">사용자 지갑 생성</div>
       </Link>
-    </>
+    </div>
   );
 }
 
--- client/src/wallet/UserWallet.tsx
+++ client/src/wallet/UserWallet.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 function UserWallet() {
-  return <div>UserWallet</div>;
+  const [idValue, setIdValue] = useState("");
+  const [rollValue, setRollValue] = useState("");
+
+  const onChangeTargetValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIdValue((e.target as HTMLInputElement).value);
+    console.log(idValue);
+  };
+
+  const onChangeTargetValueClient = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRollValue((e.target as HTMLInputElement).value);
+    console.log(rollValue);
+  };
+
+  const onClickUserCert = () => {
+    let body = {
+      id: idValue,
+      userrole: rollValue,
+    };
+    console.log(body);
+    axios
+      .post("http://localhost:8080/user/", body)
+      .then(res => console.log(res));
+  };
+
+  return (
+    <div className="container">
+      <br />
+      <h1>사용자 인증서 발급 페이지</h1>
+      <p>관리자 인증을 위한 ID와 Role을 입력하시오.</p>
+      <br />
+      <label className="form-label">ID</label>
+      <input
+        type="text"
+        className="form-control"
+        onChange={onChangeTargetValue}
+      />
+      <br />
+      <label className="form-label">ROLE</label>
+      <input
+        type="text"
+        className="form-control"
+        onChange={onChangeTargetValueClient}
+      />
+      <br />
+      <button className="btn btn-primary" onClick={onClickUserCert}>
+        사용자 인증서 생성
+      </button>
+    </div>
+  );
 }
 
 export default UserWallet;
